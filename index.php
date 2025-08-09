8ddd266d544ef23731c2d421c24e4d942ee2facb
Kyle-Gordon <gordon.kyle1@student.oaklandcc.edu>
2025-08-09 14:49:51 -0400

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+session_start();
 echo "🚀 Chef Unite backend is live!";
+if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
+    header("Location: home.php");
+} else {
+    header("Location: SignIn.php");
+}
+exit();
 ?>
