ba2c6e815c86b765e7b356287ffc4f83cb827f32
7ala Maher <halaelbayoumi@gmail.com>
2025-08-10 16:17:14 -0400

    Your commit message

--- profile.php
+++ profile.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['username']['user_id'])) {
     exit();
 }
 
-$user_id = $_SESSION['username']['user_id'];
+$user_id = $_SESSION['user_id'];
 
 // Fetch user profile details
 $sql = "SELECT username, created_at, country FROM users WHERE user_id = ?";
